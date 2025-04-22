e44af263062ee0e430269dfb1f320b7b5d34bbed
Logan Wenzel <lwenzel90@gmail.com>
2025-04-22 17:50:12 -0400

    Removed slider

--- include/DebugWindow.hpp
+++ include/DebugWindow.hpp
@@ -18,36 +18,11 @@ public:
     void update(Player& player, Attack& attack);
     void processEvents();
     void render();
-    
-    void createSlider(const std::string& name, float min, float max, float& value, 
-                     const std::function<void(float)>& callback);
-    void createPlayerControls(Player& player);
-    void createAttackControls(Attack& attack);
+    void loadFont(); // Load font from file
 
 private:
-    struct Slider {
-        std::string name;
-        float min;
-        float max;
-        float* value;
-        std::function<void(float)> callback;
-        bool active;
-        
-        sf::Text label;
-        sf::RectangleShape bar;
-        sf::CircleShape handle;
-        sf::Text valueText;
-    };
-
     bool m_isOpen = false;
     std::unique_ptr<sf::RenderWindow> window;
     sf::Font font;
-    sf::Text m_text; // Added missing text member
-    std::vector<Slider> sliders;
-    bool controlsCreated = false; // Add this flag
-    
-    const float padding = 20.f;
-    const float sliderWidth = 200.f;
-    const float sliderHeight = 4.f;
-    const float sliderSpacing = 50.f;
+    sf::Text m_text;
 };
